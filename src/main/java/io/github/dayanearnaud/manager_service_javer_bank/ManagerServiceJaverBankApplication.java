2ea6f8ae23c25f5c62f5bdf82d6beff66846bcda
Dayane Arnaud <dayanearnaud@Dayanes-MacBook-Pro.local>
2024-07-14 13:29:50 -0300

    Creating the initial structure to the project

---
+++ src/main/java/io/github/dayanearnaud/manager_service_javer_bank/ManagerServiceJaverBankApplication.java
@@ -0,0 +1,13 @@
+package io.github.dayanearnaud.manager_service_javer_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerServiceJaverBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerServiceJaverBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/dayanearnaud/manager_service_javer_bank/ManagerServiceJaverBankApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dayanearnaud.manager_service_javer_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerServiceJaverBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
