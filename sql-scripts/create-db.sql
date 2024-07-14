ab7edf0c4a142f33bd5293788f635a61d0aec85c
Dayane Arnaud <dayanearnaud@Dayanes-MacBook-Pro.local>
2024-07-14 16:59:58 -0300

    [DOCS}: SQL script updated.

--- sql-scripts/create-db.sql
+++ sql-scripts/create-db.sql
@@ -5,7 +5,7 @@ USE `javer_bank_db`;
 DROP TABLE IF EXISTS `customer`;
 
 CREATE TABLE `customer` (
-  `id` BYNARY(16) NOT NULL PRIMARY KEY,
+  `id` BINARY(16) NOT NULL PRIMARY KEY,
   `name` varchar(255),
   `phone` BIGINT,
   `email` varchar(255),
